c2c7fe0ce0dc17e431697a4e1a9491351274d96b
LucasPaquet <lucas.paquet@student.hepl.be>
2023-09-23 09:29:13 +0200

    Adding the Online Vegetables Shopping Protocol

--- lib/testClient.cpp
+++ lib/testClient.cpp
@@ -2,81 +2,177 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
 #include "tcp.h"
 
-typedef struct {
-    char nom[20];
-    int age;
-    float poids;
-} PERSONNE;
+int sClient;
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
+void HandlerSIGINT(int s);
+void Echange(char* requete, char* reponse);
+bool OVESP_Login(const char* user, const char* password);
+void OVESP_Logout();
+void OVESP_Operation(char op, int a, int b);
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3)
+    {
         printf("Erreur...\n");
-        printf("USAGE : ClientTest ipServeur portServeur\n");
+        printf("USAGE : Client ipServeur portServeur\n");
         exit(1);
     }
 
-    int sClient;
+    // Armement des signaux
+    struct sigaction A;
+    A.sa_flags = 0;
+    sigemptyset(&A.sa_mask);
+    A.sa_handler = HandlerSIGINT;
 
-    if ((sClient = ClientSocket(argv[1], atoi(argv[2]))) == -1) {
+    if (sigaction(SIGINT, &A, NULL) == -1)
+    {
+        perror("Erreur de sigaction");
+        exit(1);
+    }
+
+    // Connexion sur le serveur
+    if ((sClient = ClientSocket(argv[1], atoi(argv[2]))) == -1)
+    {
         perror("Erreur de ClientSocket");
         exit(1);
     }
 
-    // ***** Envoi de texte pur ***************************************
-    char texte[80];
-    sprintf(texte, "Bonjour, comment vas-tu ?");
-    int nbEcrits;
+    printf("Connecté sur le serveur.\n");
 
-    if ((nbEcrits = Send(sClient, texte, strlen(texte))) == -1) {
-        perror("Erreur de Send");
-        close(sClient);
+    // Phase de login
+    char user[50], password[50];
+    printf("user: ");
+    fgets(user, 50, stdin);
+    user[strlen(user) - 1] = 0;
+    printf("password: ");
+    fgets(password, 50, stdin);
+    password[strlen(password) - 1] = 0;
+
+    if (!OVESP_Login(user, password))
         exit(1);
+
+    while (1)
+    {
+        int a, b;
+        char op;
+        printf("Operation (<CTRL-C> pour fin) : ");
+        fflush(stdin);
+        scanf("%d %c %d", &a, &op, &b); // pas optimal !
+        OVESP_Operation(op, a, b);
     }
+}
 
-    printf("NbEcrits = %d\n", nbEcrits);
-    printf("Ecrit = --%s--\n", texte);
-
-    // ***** Reception texte pur **************************************
-    char buffer[100];
-    int nbLus;
-
-    if ((nbLus = Receive(sClient, buffer)) < 0) {
-        perror("Erreur de Receive");
-        close(sClient);
-        exit(1);
-    }
-
-    printf("NbLus = %d\n", nbLus);
-    buffer[nbLus] = 0;
-    printf("Lu = --%s--\n", buffer);
-
-    // ***** Envoi d'une structure ************************************
-    PERSONNE p;
-    strcpy(p.nom, "Wagner");
-    p.age = 49;
-    p.poids = 87.21f;
-
-    if ((nbEcrits = Send(sClient, (char*)&p, sizeof(PERSONNE))) < 0) {
-        perror("Erreur de Send");
-        close(sClient);
-        exit(1);
-    }
-
-    printf("NbEcrits = %d\n", nbEcrits);
-    printf("Ecrit = --%s--%d--%f--\n", p.nom, p.age, p.poids);
-
-    // ***** Reception d'une structure ********************************
-    if ((nbLus = Receive(sClient, (char*)&p)) < 0) {
-        perror("Erreur de Receive");
-        close(sClient);
-        exit(1);
-    }
-
-    printf("NbLus = %d\n", nbLus);
-    printf("Lu = --%s--%d--%f--\n", p.nom, p.age, p.poids);
-
+// ***** Fin de connexion ********************************************
+void HandlerSIGINT(int s)
+{
+    printf("\nArrêt du client.\n");
+    OVESP_Logout();
     close(sClient);
     exit(0);
+}
+
+// ***** Gestion du protocole OVESP ***********************************
+bool OVESP_Login(const char* user, const char* password)
+{
+    char requete[200], reponse[200];
+    bool onContinue = true;
+
+    // ***** Construction de la requête *********************
+    sprintf(requete, "LOGIN#%s#%s", user, password);
+
+    // ***** Envoi requête + réception réponse **************
+    Echange(requete, reponse);
+
+    // ***** Parsing de la réponse **************************
+    char* ptr = strtok(reponse, "#"); // entête = LOGIN (normalement...)
+    ptr = strtok(NULL, "#");          // statut = ok ou ko
+
+    if (strcmp(ptr, "ok") == 0)
+        printf("Login OK.\n");
+    else
+    {
+        ptr = strtok(NULL, "#"); // raison du ko
+        printf("Erreur de login: %s\n", ptr);
+        onContinue = false;
+    }
+
+    return onContinue;
+}
+
+//*******************************************************************
+void OVESP_Logout()
+{
+    char requete[200], reponse[200];
+    int nbEcrits, nbLus;
+
+    // ***** Construction de la requête *********************
+    sprintf(requete, "LOGOUT");
+
+    // ***** Envoi requête + réception réponse **************
+    Echange(requete, reponse);
+
+    // ***** Parsing de la réponse **************************
+    // Pas vraiment utile...
+}
+
+//*******************************************************************
+void OVESP_Operation(char op, int a, int b)
+{
+    char requete[200], reponse[200];
+
+    // ***** Construction de la requête *********************
+    sprintf(requete, "OPER#%c#%d#%d", op, a, b);
+
+    // ***** Envoi requête + réception réponse **************
+    Echange(requete, reponse);
+
+    // ***** Parsing de la réponse **************************
+    char* ptr = strtok(reponse, "#"); // entête = OPER (normalement...)
+    ptr = strtok(NULL, "#");          // statut = ok ou ko
+
+    if (strcmp(ptr, "ok") == 0)
+    {
+        ptr = strtok(NULL, "#"); // résultat du calcul
+        printf("Résultat = %s\n", ptr);
+    }
+    else
+    {
+        ptr = strtok(NULL, "#"); // raison du ko
+        printf("Erreur: %s\n", ptr);
+    }
+}
+
+//***** Échange de données entre client et serveur ******************
+void Echange(char* requete, char* reponse)
+{
+    int nbEcrits, nbLus;
+
+    // ***** Envoi de la requête ****************************
+    if ((nbEcrits = Send(sClient, requete, strlen(requete))) == -1)
+    {
+        perror("Erreur de Send");
+        close(sClient);
+        exit(1);
+    }
+
+    // ***** Attente de la réponse **************************
+    if ((nbLus = Receive(sClient, reponse)) < 0)
+    {
+        perror("Erreur de Receive");
+        close(sClient);
+        exit(1);
+    }
+
+    if (nbLus == 0)
+    {
+        printf("Serveur arrêté, pas de réponse reçue...\n");
+        close(sClient);
+        exit(1);
+    }
+
+    reponse[nbLus] = 0;
 }
