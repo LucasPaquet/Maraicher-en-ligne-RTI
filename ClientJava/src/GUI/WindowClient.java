e61277bdfc23cd8e20373d5dca012e3d2c5894b9
Lucas Paquet <paquet.l03@gmail.com>
2023-10-12 12:04:20 +0200

    Added OVESP_Acaht() and OVESP_Caddie()

--- ClientJava/src/GUI/WindowClient.java
+++ ClientJava/src/GUI/WindowClient.java
@@ -3,6 +3,7 @@ package GUI;
 import Tcp.TcpConnection;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -15,7 +16,7 @@ public class WindowClient extends JFrame {
     private JButton btnLogout;
     private JCheckBox cbNewClient;
     private JPanel JPanePublicite;
-    private JTextField textField7;
+    private JTextField tfTotal;
     private JButton btnDelete;
     private JButton btnConfirm;
     private JButton btnVider;
@@ -40,13 +41,29 @@ public class WindowClient extends JFrame {
 
         articleEnCours = 1;
 
+        DefaultTableModel model = new DefaultTableModel();
+
+        // Définir les noms des colonnes
+        model.setColumnIdentifiers(new String[]{"Article", "Prix à l'unité", "Quantité"});
+        model.addRow(new Object[]{"Orange", "1,12", "12"});
+
+        // Appliquer le modèle de tableau à la JTable
+        tablePanier.setModel(model);
+
+        // changer les proprietes de la JTable
+        tablePanier.setDefaultEditor(Object.class, null);
+        tablePanier.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        tablePanier.setRowSelectionAllowed(true);
+        tablePanier.setColumnSelectionAllowed(false);
+
+
         setTitle("Le Maraicher en ligne");
         setIconImage(new ImageIcon(this.getClass().getResource("/Images/icon.png")).getImage()); // pour ajouter une icone a l'app
         setContentPane(JPanelMain);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // pour implementer les fonctions de fenetre de bas (exit, agrandir, ...)
         pack();
 
-        serverConnection = new TcpConnection("192.168.28.128", 50000);
+        serverConnection = new TcpConnection("192.168.230.128", 50000);
 
         //********** Fermer la fenetre ***************
         addWindowListener(new WindowAdapter() {
@@ -88,7 +105,15 @@ public class WindowClient extends JFrame {
         btnBuy.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                OVESP_Consult(articleEnCours);
+                if (tfQuantite.getText().isEmpty()){
+                    JOptionPane.showMessageDialog(null, "Entrez une quantité", "Erreur d'achat", JOptionPane.ERROR_MESSAGE);
+                }
+                else {
+                    if (OVESP_Achat()){
+                        OVESP_Caddie();
+                        OVESP_Consult(articleEnCours);
+                    }
+                }
             }
         });
     }
@@ -238,5 +263,72 @@ public class WindowClient extends JFrame {
             articleEnCours = Integer.parseInt(champs[2]);
             setArticle(champs[3],champs[4],champs[5],champs[6]);
         }
+    }
+
+    /**
+     * Permet de d'acheter un article. Si article non trouvé, retour -1. Si trouvé mais que stock insuffisant, retour d’une quantité 0 → Si ok, le stock est mis à jour en BD et le contenu du caddie est mémorisé au niveau du serveur → actuellement aucune action sur tables factures et ventes
+     */
+    public boolean OVESP_Achat(){
+        String data;
+        String response;
+
+        // Envoie de la requete
+        data = "ACHAT#" + articleEnCours + "#" + tfQuantite.getText();
+        serverConnection.send(data);
+
+        // Reception et parsing de la reponse
+        response = serverConnection.receive();
+        String[] champs = response.split("#");
+
+        if (champs[1].equals("ok")){
+            JOptionPane.showMessageDialog(null, champs[2] + " articles["+ champs[3] + "] ont été ajoutés à votre panier", "Achat réussie", JOptionPane.INFORMATION_MESSAGE);
+            return true;
+        }
+        else{
+            switch (Integer.parseInt(champs[2])){
+                case 0: JOptionPane.showMessageDialog(null, "Stock insufisant","Erreur d'achat", JOptionPane.ERROR_MESSAGE);
+                    break;
+                case -1: JOptionPane.showMessageDialog(null, "Article non trouvé","Erreur d'achat", JOptionPane.ERROR_MESSAGE);
+                    break;
+                case -2: JOptionPane.showMessageDialog(null, "Votre panier est plein !","Erreur d'achat", JOptionPane.ERROR_MESSAGE);
+                    break;
+            }
+
+            return false;
+        }
+    }
+
+    /**
+     * Retourne l’entièreté du contenu du caddie au client
+     */
+    public void OVESP_Caddie(){
+        String data;
+        String response;
+        int nbArticle;
+        float prixTotal = 0;
+
+        // Envoie de la requete
+        data = "CADDIE" ;
+        serverConnection.send(data);
+
+        // Reception et parsing de la reponse
+        response = serverConnection.receive();
+        String[] champs = response.split("#");
+
+        nbArticle = Integer.parseInt(champs[1]);
+
+
+        DefaultTableModel model = new DefaultTableModel();
+        model.setColumnIdentifiers(new String[]{"Article", "Prix à l'unité", "Quantité"});
+
+        for (int i = 0; i<nbArticle; i++){
+            // Définir les noms des colonnes
+            model.addRow(new Object[]{champs[(i*5)+3], champs[(i*5)+5], champs[(i*5)+4]});
+            prixTotal += Float.parseFloat(champs[(i*5)+5]) * Integer.parseInt(champs[(i*5)+4]);
+        }
+
+        tfTotal.setText(String.valueOf(prixTotal));
+        tablePanier.setModel(model);
+
     }
 }
