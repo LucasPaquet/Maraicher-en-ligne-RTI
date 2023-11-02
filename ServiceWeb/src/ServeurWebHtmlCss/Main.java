3a8c3b0d81caae3c038b9a3d76e65d2dd15e84a6
LucasPaquet <113984975+LucasPaquet@users.noreply.github.com>
2023-11-02 15:07:16 +0100

    Start step 3

---
+++ ServiceWeb/src/ServeurWebHtmlCss/Main.java
@@ -0,0 +1,28 @@
+package ServeurWebHtmlCss;
+
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        HttpServer serveur = null;
+        try
+        {
+            serveur = HttpServer.create(new InetSocketAddress(8080),0);
+            serveur.createContext("/",new HandlerHtml());
+            serveur.createContext("/css",new HandlerCss());
+            /*
+            serveur.createContext("/images",new HandlerImages());
+            serveur.createContext("/pdfs",new HandlerPdfs());
+             */
+            System.out.println("Demarrage du serveur HTTP...");
+            serveur.start();
+        }
+        catch (IOException e)
+        {
+            System.out.println("Erreur: " + e.getMessage());
+        }
+    }
+}
